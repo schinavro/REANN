9855334dbd6e9b0aeb0ff8f6c0b00fd9f5e011c7
schinavro <schinavro@gmail.com>
2022-10-24 19:38:52 +0900

    Optimize RMSE

--- reann/src/Loss.py
+++ reann/src/Loss.py
@@ -1,7 +1,7 @@
 import torch 
 import torch.nn as nn
 
-class Loss2(nn.Module):
+class Loss(nn.Module):
    def __init__(self):
       super(Loss, self).__init__()
       self.loss_fn=nn.MSELoss(reduction="sum")
@@ -9,16 +9,22 @@ class Loss2(nn.Module):
    def forward(self,var,ab): 
       return  torch.cat([self.loss_fn(ivar,iab).view(-1) for ivar, iab in zip(var,ab)])
 
-class Loss(nn.Module):
+import torch as tc
+class MSEFLoss(nn.Module):
    def __init__(self):
-      super(Loss2, self).__init__()
-      
-   def forward(self, var, ab):
-      self.record = []
-      errs = []
-      for ivar, iab in zip(var, ab):
-         error = (ivar - iab)*(ivar - iab)
-         self.record.append(error.cpu().detach())
-         errs.append(torch.sum(error))
-      return torch.cat(errs)
-      
+      super(MSEFLoss, self).__init__()
+      # self.muE = muE
+      # self.muF = muF
+
+   def forward(self, var, ab, numatoms=None):
+
+      y1 = [i for i in var]
+      y0 = [i for i in ab]
+      NC = len(y1[0])
+      NTA = len(y1[1])
+
+      ee = (y1[0] - y0[0])/numatoms
+      ff = y1[1] - y0[1]
+      self.lossE = tc.sum(ee*ee).view(-1)
+      self.lossG = tc.sum(ff*ff).view(-1)
+      return torch.cat([self.lossE/NC, self.lossG/NTA])
--- reann/src/optimize.py
+++ reann/src/optimize.py
@@ -10,14 +10,18 @@ data_train,data_test,Prop_class,loss_fn,optim,scheduler,ema,restart,PES_Normal,d
     rank=dist.get_rank()
     best_loss=1e30*torch.ones(1,device=device)    
 
+    optim.param_groups[0]["lr"] = start_lr
+
+    fout.write("{0:<5s} {1:>8s} {2:5s} {3:>16s} {4:>16s} {5:>5s} {6:>16s} {7:>16s} \n".format(
+                "Epoch", "lr", "Train", "RMSE-E(meV/atom)", "RMSE-F(meV/å)", " Test", "RMSE-E(meV/atom)", "RMSE-F(meV/atom)"))
     for iepoch in range(Epoch): 
         # set the model to train
        Prop_class.train()
-       lossprop=torch.zeros(nprop,device=device)        
+       lossprop=torch.zeros(nprop+2,device=device)        
+       # NC, NTA, lossE, lossG = 0, 0, 0, 0
        for data in data_train:
           abProp,cart,numatoms,species,atom_index,shifts=data
-          loss=loss_fn(Prop_class(cart,numatoms,species,atom_index,shifts),abProp)
-          lossprop+=loss.detach()
+          loss=loss_fn(Prop_class(cart,numatoms,species,atom_index,shifts),abProp, numatoms)
           loss=torch.sum(torch.mul(loss,prop_ceff[0:nprop]))
           # clear the gradients of param
           #optim.zero_grad()
@@ -27,46 +31,66 @@ data_train,data_test,Prop_class,loss_fn,optim,scheduler,ema,restart,PES_Normal,d
           loss.backward()
           optim.step()   
 
+          if np.mod(iepoch,print_epoch)==0:
+              lossprop[0] += len(numatoms)
+              lossprop[1] += torch.sum(numatoms)
+              lossprop[2] += loss_fn.lossE.item()
+              lossprop[3] += loss_fn.lossG.item()
+
+          # lossprop += loss_fn.detach()
           #doing the exponential moving average update the EMA parameters
-          ema.update()
+          #ema.update()
     
        #  print the error of vailadation and test each print_epoch
        if np.mod(iepoch,print_epoch)==0:
           # apply the EMA parameters to evaluate
-          ema.apply_shadow()
+          # ema.apply_shadow()
           # set the model to eval for used in the model
           Prop_class.eval()
           # all_reduce the rmse form the training process 
           # here we dont need to recalculate the training error for saving the computation
-          dist.all_reduce(lossprop,op=dist.ReduceOp.SUM)
+          dist.all_reduce(lossprop, op=dist.ReduceOp.SUM)
+          NC, NTA, lossE, lossG = lossprop
+          loss = torch.tensor([torch.sqrt(lossE / NC), torch.sqrt(lossG / NTA)])
           
           # get the current rank and print the error in rank 0
           if rank==0:
-              lossprop=torch.sqrt(lossprop.detach().cpu()/train_nele)
+              # lossprop=torch.sqrt(lossprop.detach().cpu())
               lr=optim.param_groups[0]["lr"]
-              fout.write("{:5} {:4} {:15} {:5e}  {} ".format("Epoch=",iepoch,"learning rate",lr,"train error:"))
-              for error in lossprop:
-                  fout.write('{:10.5f} '.format(error))
+              fout.write("{0:>4d}: {1:>8.2e} {2:5s} {3:>16.8f} {4:>16.8f}".format(iepoch,lr,' ',loss[0]*1000, loss[1]*1000))
+              #fout.write('{:10.5f} '.format(loss[0]*1000))
+              #fout.write('{:10.5f} '.format(loss[1]*1000))
+              #for error in lossprop:
+              #    fout.write('{:10.5f} '.format(error))
           
           # calculate the test error
-          lossprop=torch.zeros(nprop,device=device)
+          lossprop=torch.zeros(nprop+2,device=device)
           for data in data_test:
              abProp,cart,numatoms,species,atom_index,shifts=data
              loss=loss_fn(Prop_class(cart,numatoms,species,atom_index,shifts,\
-             create_graph=False),abProp)
-             lossprop=lossprop+loss.detach()
-             print(lossprop)
+             create_graph=False),abProp, numatoms=numatoms)
+             
+             lossprop[0] += len(numatoms)
+             lossprop[1] += torch.sum(numatoms)
+             lossprop[2] += loss_fn.lossE.item()
+             lossprop[3] += loss_fn.lossG.item()
+
+             # lossprop=lossprop+loss.detach()
 
           # all_reduce the rmse
           dist.all_reduce(lossprop.detach(),op=dist.ReduceOp.SUM)
-          loss=torch.sum(torch.mul(lossprop,prop_ceff[0:nprop]))
-          scheduler.step(loss)
+          NC, NTA, lossE, lossG = lossprop
+          loss = torch.tensor([torch.sqrt(lossE / NC), torch.sqrt(lossG / NTA)]).to(device=device)
+          tot_loss = torch.sum(loss*prop_ceff[0:nprop])
+
+          # loss=torch.sum(torch.mul(lossprop,prop_ceff[0:nprop]))
+          # scheduler.step(tot_loss)
           lr=optim.param_groups[0]["lr"]
-          f_ceff=init_f+(final_f-init_f)*(lr-start_lr)/(end_lr-start_lr+1e-8)
-          prop_ceff[1]=f_ceff
+          # f_ceff=init_f+(final_f-init_f)*(lr-start_lr)/(end_lr-start_lr+1e-8)
+          # prop_ceff[1]=f_ceff
           #  save the best model
-          if loss<best_loss[0]:
-             best_loss[0]=loss
+          if tot_loss <best_loss[0]:
+             best_loss[0]=tot_loss
              if rank == 0:
                  state = {'reannparam': Prop_class.state_dict(), 'optimizer': optim.state_dict()}
                  torch.save(state, "./REANN.pth")
@@ -77,19 +101,19 @@ data_train,data_test,Prop_class,loss_fn,optim,scheduler,ema,restart,PES_Normal,d
              # for example, when process 0  is saving the model to generate "REANN.pth" and the other processes are reading the "REANN.pth".
           
           # restore the model for continue training
-          ema.restore()
+          # ema.restore()
           # back to the best error
-          if loss>25*best_loss[0] or loss.isnan():
-              dist.barrier()
-              restart(Prop_class,"REANN.pth")
-              optim.param_groups[0]["lr"]=optim.param_groups[0]["lr"]*decay_factor
-              ema.restart()
+          #if tot_loss>25*best_loss[0] or tot_loss.isnan():
+          #    dist.barrier()
+          #    restart(Prop_class,"REANN.pth")
+          #    # optim.param_groups[0]["lr"]=optim.param_groups[0]["lr"]*decay_factor
+          #    # ema.restart()
 
           if rank==0:
-              lossprop=torch.sqrt(lossprop.detach().cpu()/test_nele)
-              fout.write('{} '.format("test error:"))
-              for error in lossprop:
-                 fout.write('{:10.5f} '.format(error))
+              # lossprop=torch.sqrt(lossprop.detach().cpu()/test_nele)
+              fout.write("{0:6s} {1:>16.8f} {2:>16.8f}".format(' ', loss[0]*1000, loss[1]*1000))
+              # for error in lossprop:
+              #    fout.write('{:10.5f} '.format(error))
               # if stop criterion
               fout.write("\n")
               fout.flush()
